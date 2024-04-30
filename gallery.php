a7fd9a75b79d5c8cb65a0e52e9b79abbfaa14912
Kris Mannino <krismannino@gmail.com>
2024-04-30 16:43:11 -0400

    added github link

--- gallery.php
+++ gallery.php
@@ -29,7 +29,7 @@
           </button>
           </li>
           <li class="flex items-center">
-            <a href="https://krismannino.com/" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+            <a href="https://github.com/KrisMannino" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
           </li>
         
           <li class="flex items-center">
